157f886fb8d311df345d813418ed4b3814e4bc7b
Vision System <embedded@visionsystem.it>
2024-08-16 09:28:51 +0200

    added more sensor readings to tutorial 03

--- 03-sense_hat/imu_example.py
+++ 03-sense_hat/imu_example.py
@@ -7,12 +7,15 @@ sense = SenseHat()
 # Get the orientation, temperature, and humidity
 while True:
     orientation = sense.get_orientation_degrees()
-    print(orientation)
+    print("Orientation:", orientation)
 
     temperature = sense.get_temperature()
-    print(temperature)
+    print("Temperature:", temperature)
     
     humidity = sense.get_humidity()
-    print(humidity)
+    print("Humidity:", humidity)
+
+    pressure = sense.get_pressure()
+    print("Pressure:", pressure)
     
     time.sleep(0.2)
